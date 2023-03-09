49ddd6d11cec48a06586f2be3282c09797396d3a
Artur Ishmurzin <89225651308ia@gmail.com>
2023-03-09 23:12:53 +0500

    Add project files.

---
+++ ConsoleApp1/ArrayExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otus.ArraySum
+{
+    public static class ArrayExtensions
+    {
+        public static void FillRandom(this int[] array, int min, int max)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(min, max);
+            }
+        }
+
+        public static long CustomSum(this int[] array)
+        {
+            long result = 0;
+
+            foreach (int item in array)
+            {
+                result += item;
+            }
+
+            return result;
+        }
+
+        public async static Task<long> ParallelSum(this int[] array, int threadCount)
+        {
+            var tasks = new List<Task<long>>();
+
+            Func<int, int, long> sum = (int start, int end) =>
+            {
+                long result = 0;
+
+                for (int i = start; i < Math.Min(array.Length, end); i++)
+                {
+                    result += array[i];
+                }
+
+                return result;
+            };
+
+            int batchSize = array.Length / threadCount;
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                int start = i * batchSize;
+                int end = start + batchSize;
+                tasks.Add(Task.Run<long>(() => sum(start, end)));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            long result = 0;
+
+            foreach (var item in tasks)
+            {
+                result += await item;
+            }
+
+            return result;
+        }
+
+        public static long ParallelSumWithLinq(this int[] array)
+        {
+            return array.AsParallel().Aggregate<int, long, long>(
+            0,
+            (acc, val) => acc + val,
+            (acc1, acc2) => acc1 + acc2,
+            acc => (long)acc
+            );
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Otus.ArraySum;
+using System.Diagnostics;
+
+int arrayLength = 100000000;
+
+var array = new int[arrayLength];
+
+array.FillRandom(1, 50);
+
+var stopwatch = Stopwatch.StartNew();
+
+var result = array.CustomSum();
+
+stopwatch.Stop();
+
+Console.WriteLine(result);
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+stopwatch = Stopwatch.StartNew();
+result = await array.ParallelSum(5);
+stopwatch.Stop();
+Console.WriteLine(result);
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+stopwatch = Stopwatch.StartNew();
+result = array.ParallelSumWithLinq();
+
+stopwatch.Stop();
+Console.WriteLine(result);
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+Console.ReadLine();
